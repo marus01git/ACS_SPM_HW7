899e2d77198379a320da845a18bec43481aff6ba
marus01git <marus01@pfw.edu>
2024-03-01 14:07:43 -0600

    Design a data model for storing previous sprints' points & Design a data model for team member details

---
+++ FeatureA.py
@@ -0,0 +1,4 @@
+# Subtask A1: Define a Data Structure for Storing Sprint Points
+# This will be a simple list to store points from each sprint.
+
+sprint_points = [10, 12, 8, 14]  # Example data
---
+++ FeatureB.py
@@ -0,0 +1,6 @@
+# This will be a list of dictionaries, with each dictionary representing a team member's details.
+team_members = [
+    {"name": "Alice", "pto_hours": 8, "ceremony_hours": 2, "daily_hours": 8},
+    {"name": "Bob", "pto_hours": 0, "ceremony_hours": 2, "daily_hours": 8},
+    
+]
